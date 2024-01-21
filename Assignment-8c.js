fa976bd3f859e607161797464e9e64c6286c6d78
nofopkc <155906819+nofopkc@users.noreply.github.com>
2024-01-21 12:28:47 -0800

    Add files via upload

---
+++ Assignment-8c.js
@@ -0,0 +1,5 @@
+let Length=prompt("Please Enter the Length of a Rectangle");
+let Width=prompt("Please Enter the width of a Rectangle");
+Area=Length*Width;
+
+console.log("The area of the Rectangle = ",Area,"Sq Units");
